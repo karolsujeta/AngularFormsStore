951250acc4286271161389b82ce9525a93b580fa
unknown <karol.sujeta@outlook.com>
2020-02-04 17:45:52 +0100

    app

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-forms',
@@ -26,8 +27,64 @@ export class FormsComponent implements OnInit {
       lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
       address: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
       sex: ['', Validators.required],
-      age: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(15)]],
+      age: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(3)]],
     });
+
+    this.modelForm.valueChanges.pipe(debounceTime(1000)).subscribe((value) => {
+      this.onControlValueChanged();
+    });
+
+    this.onControlValueChanged(); // ustawiamy początkowany stan walidacji
+  }
+
+  formErrors = {
+    name: '',
+    lastname: '',
+    address: '',
+    sex: '',
+    age: ''
+  }
+
+  private validationMessage = {
+    name: {
+      required: "Wprowadź imię!",
+      minlength: "Co najmniej 2 litery!",
+      maxlength: "Maksymalnie 15 znaków!",
+    },
+    lastname: {
+      required: "Wprowadź nazwisko!",
+      minlength: "Co najmniej 2 litery!",
+      maxlength: "Maksymalnie 15 znaków!",
+    },
+    address: {
+      required: "Wprowadź adres!",
+      minlength: "Co najmniej 2 znaki!",
+      maxlength: "Maksymalnie 25 znaków!",
+    },
+    sex: {
+      required: "Wprowadź płeć: Mężczyzna/Kobieta",
+    },
+    age: {
+      required: "Wprowadź wiek!",
+      minlength: "Wprowadź najmniej 1 cyfrę!",
+      maxlength: "Maksymalnie 3 znaki!",
+    },
+  }
+
+  onControlValueChanged() {
+    const form = this.modelForm;
+
+    for (let field in this.formErrors) {
+      this.formErrors[field] = '';
+      let control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const validationMessage = this.validationMessage[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += validationMessage[key] + ' ';
+        }
+      }
+    }
   }
 
   addTodo() {
